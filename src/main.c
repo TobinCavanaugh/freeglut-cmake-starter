eef78a1078c409cdfbf4952ddaabb6e66f83eac0
dante1130 <andrewdante2@gmail.com>
2024-03-10 00:24:05 +0800

    Fix narrowing warnings

--- src/main.c
+++ src/main.c
@@ -7,14 +7,14 @@ void display(void) {
 
     glBegin(GL_POLYGON);
     {
-        glColor3f(1, 0, 0);
-        glVertex3f(-0.6, -0.75, 0.5);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(-0.6f, -0.75f, 0.5f);
 
-        glColor3f(0, 1, 0);
-        glVertex3f(0.6, -0.75, 0);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(0.6f, -0.75f, 0.0f);
 
-        glColor3f(0, 0, 1);
-        glVertex3f(0, 0.75, 0);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(0.0f, 0.75f, 0.0f);
     }
     glEnd();
 
